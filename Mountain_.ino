6ff69da73731c0f85b3bcc6381d625f87b39041a
jma17 <jma17@punahou.edu>
2015-01-07 22:25:26 -1000

    worked a lot on game.
    I really worked hard on this game and it is just about done. I added a
    win screen and a loose screen. Mr Kiang really helped me through some
    of the difficult parts. Still need to indent and comment code.

--- Mountain_.ino
+++ Mountain_.ino
@@ -53,6 +53,7 @@ boolean movingRight[8]; // the values in this array provide the direction each o
 int level = 0;
 int win = 0;
 int loose = 0;
+boolean gameOver = false;
 
 void setup()                    // run once, when the sketch starts
 {
@@ -65,34 +66,55 @@ Serial.begin(9600);
 
 void loop()                     // run over and over again
 {
-DrawPx(0,6,White);
+  if (gameOver == false)
+    play();
+  else 
+  {
+    if (level > 7)
+    {
+      winScreen();
+    }
+    else
+    {
+      loseScreen();
+    }
+   NewGame();
+  }
+}
+
+void play()
+{
+ DrawPx(0,6,White);
  DrawPx(0,7,White);
- DrawPx(7,6,White);
- DrawPx(7,7,White);
+ 
+ DrawPx(0,4,Green);
+ DrawPx(0,5,Green);
+ DrawPx(0,3,Green);
   DrawArray();
   DisplaySlate();
-  delay(100);
+  delay(60);
   ClearSlate();
   CheckButtonsPress();
   if (Button_A)
   {  
-    level++;    //stop dots
-    rows[level]++;  //have dots appear above
-  }  
+    //if dots line up
+    if (lineUp() == true)
+    {
+     level++;    //stop dots
+     rows[level]++;  //have dots appear above
+     Tone_Start(9000, 50);
+    }
+    else
+    {
+     level++;    //stop dots
+     rows[level]++;  //
+     gameOver = true;
+    }
+  }
   UpdateArray();
   
-  if (level > 7)
-  {
-   win = 1;
-  }
-  if (win > 0) 
-    {
-    winscreen();
-    NewGame();
-    }
-    
 }
-
+  
 void DrawArray() // This draws three-dot segments and then two-dot segments.
 {
   for (int i = 0; i < 3; i++) // First three rows are three-dot segments
@@ -102,10 +124,11 @@ void DrawArray() // This draws three-dot segments and then two-dot segments.
       DrawPx(rows[i]-1,i,1); // Dot to left
       DrawPx(rows[i],i,1); // Reference dot
       DrawPx(rows[i]+1,i,1); // Dot to right
+      
     }
   }
   
-  for (int i = 3; i < 6; i++) // Upper rows are two-dot segments
+  for (int i = 3; i < 9; i++) // Upper rows are two-dot segments
   {
     if (rows[i] > 0) // if there is something in the row
     {
@@ -114,14 +137,7 @@ void DrawArray() // This draws three-dot segments and then two-dot segments.
     }
   }
   
-  for (int i = 6; i < 9; i++) // Upper rows are one-dot segments
-  {
-    if (rows[i] > 0) // if there is something in the row
-    {
-      DrawPx(rows[i],i,1); // Reference dot
-      
-    }
-  }
+ 
 }
 
 void UpdateArray()
@@ -196,8 +212,148 @@ void PrintArray()
  
 }
 
-void winscreen()
+void winScreen()
   {
+          ClearSlate();
+          DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          DrawPx(5,0,Green);
+          DrawPx(6,0,Green);
+          DrawPx(7,0,Green);  
+          DrawPx(2,1,White);
+          DrawPx(3,1,White);
+          DrawPx(4,1,White);
+          DrawPx(5,1,White);
+          DrawPx(3,2,Blue);
+          DrawPx(5,2,Blue);
+          DrawPx(4,3,Blue);
+          DrawPx(4,4,Blue);
+          DrawPx(5,4,Blue);
+          DrawPx(3,4,Blue);    
+          DrawPx(4,5,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(18183, 50);
+          
+          DisplaySlate();
+          delay(500);
+          ClearSlate();
+           DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          DrawPx(5,0,Green);
+          DrawPx(6,0,Green);
+          DrawPx(7,0,Green);  
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(5,1,Green);
+          DrawPx(3,3,Blue);
+          DrawPx(5,3,Blue);
+          DrawPx(4,4,Blue);
+          DrawPx(4,5,Blue);
+          DrawPx(5,5,Blue);
+          DrawPx(3,5,Blue);    
+          DrawPx(4,6,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+          Tone_Start(6000, 50);
+          DisplaySlate();
+          delay(500);
+          
+          ClearSlate();
+          DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          DrawPx(5,0,Green);
+          DrawPx(6,0,Green);
+          DrawPx(7,0,Green);  
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(5,1,Green);
+          DrawPx(3,2,Blue);
+          DrawPx(5,2,Blue);
+          DrawPx(4,3,Blue);
+          DrawPx(4,4,Blue);
+          DrawPx(5,4,Blue);
+          DrawPx(3,4,Blue);    
+          DrawPx(4,5,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(9000, 50);
+          
+          DisplaySlate();
+          delay(500);
+          
+          ClearSlate();
+           DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          DrawPx(5,0,Green);
+          DrawPx(6,0,Green);
+          DrawPx(7,0,Green);  
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(5,1,Green);
+          DrawPx(3,3,Blue);
+          DrawPx(5,3,Blue);
+          DrawPx(4,4,Blue);
+          DrawPx(4,5,Blue);
+          DrawPx(5,5,Blue);
+          DrawPx(3,5,Blue);    
+          DrawPx(4,6,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+          Tone_Start(6000, 50);
+          DisplaySlate();
+          delay(500);
+          
+          
+          ClearSlate();
+          DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          DrawPx(5,0,Green);
+          DrawPx(6,0,Green);
+          DrawPx(7,0,Green);  
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(5,1,Green);
+          DrawPx(3,2,Blue);
+          DrawPx(5,2,Blue);
+          DrawPx(4,3,Blue);
+          DrawPx(4,4,Blue);
+          DrawPx(5,4,Blue);
+          DrawPx(3,4,Blue);    
+          DrawPx(4,5,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(18183, 50);
+          
+          DisplaySlate();
+          delay(500);
           ClearSlate();
           DrawPx(1,6,Green);
           DrawPx(2,6,Green);
@@ -231,26 +387,376 @@ void winscreen()
           DrawPx(3,2,Green);
           DrawPx(2,2,Green);
           Tone_Start(18183, 50);
-          Tone_Start(18183, 50);
-          Tone_Start(18183, 50);
+          delay(100);
+          Tone_Start(15000, 50);
+          delay(100);
+          Tone_Start(12000, 50);
+          delay(100);
+          Tone_Start(9000, 50);
+          delay(100);
+          Tone_Start(6000, 50);
+          delay(100);
+          
+          
           DisplaySlate();
-          delay(2000);
-          NewGame();
+          delay(1000);
   }
     
+    
+boolean lineUp()
+{
+/*
+  for the row we are on rows[level]
+  
+  if the reference point is less than the length of the segment from the previous reference point, return true.
+  Otherwise, return false.
+  */
+  if (level < 1) return true;
+  int segmentLength = 2;
+  if ( rows[level] > 2 ) 
+    segmentLength = 1;
+  
+  
+  int distance = (rows[level] - rows[level-1]);
+  if (distance < 0)
+    distance = distance * -1;
+  
+  if (distance < segmentLength) return true;
+  else return false;
+  
+}
+
     
 void NewGame()
 {
   
   ClearSlate();
+for (int i = 0; i <= level; i++)
+{
+  rows[i] = 0;
+}
+
+rows[0] = 1;
 
 level = 0;
 win = 0;
 loose = 0;
+gameOver = false;
   
   
   DisplaySlate();
  
 }    
-    
+
+void loseScreen()
+{
+   ClearSlate();
+           DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+           
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(6,6,Blue);
+          DrawPx(5,4,Blue);
+          DrawPx(7,4,Blue);
+          DrawPx(6,5,Blue);
+          DrawPx(7,6,Blue);
+          DrawPx(7,6,Blue);
+          DrawPx(5,6,Blue);    
+          DrawPx(6,7,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(3000, 50);
+          
+          DisplaySlate();
+          delay(150);
+        ClearSlate();
+           DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          
+          DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+           
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(6,5,Blue);
+          DrawPx(5,3,Blue);
+          DrawPx(7,3,Blue);
+          DrawPx(6,4,Blue);
+          DrawPx(7,5,Blue);
+          DrawPx(7,5,Blue);
+          DrawPx(5,5,Blue);    
+          DrawPx(6,6,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(5000, 50);
+          
+          DisplaySlate();
+          delay(150);
+ 
+  ClearSlate();
+           DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          
+          DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+           
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(6,4,Blue);
+          DrawPx(5,2,Blue);
+          DrawPx(7,2,Blue);
+          DrawPx(6,3,Blue);
+          DrawPx(7,4,Blue);
+          DrawPx(7,4,Blue);
+          DrawPx(5,4,Blue);    
+          DrawPx(6,5,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(7000, 50);
+          
+          DisplaySlate();
+          delay(150);
+          ClearSlate();  
+   
+          DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          
+          
+           DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(6,3,Blue);
+          DrawPx(5,1,Blue);
+          DrawPx(7,1,Blue);
+          DrawPx(5,3,Blue);
+          DrawPx(7,3,Blue);
+          DrawPx(6,2,Blue);
+          DrawPx(7,3,Blue);
+          DrawPx(7,3,Blue);
+          DrawPx(5,3,Blue);    
+          DrawPx(6,4,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(9000, 50);
+          
+          DisplaySlate();
+          delay(150);
+          ClearSlate();  
+               DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          
+          
+           DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(5,0,Blue);
+          DrawPx(7,0,Blue);
+          DrawPx(6,2,Blue);
+          DrawPx(5,2,Blue);
+          DrawPx(7,2,Blue);
+          DrawPx(6,1,Blue);
+          DrawPx(7,2,Blue);
+          DrawPx(7,2,Blue);
+          DrawPx(5,2,Blue);    
+          DrawPx(6,3,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(11000, 50);
+          
+          DisplaySlate();
+          delay(150);
+          
+          ClearSlate();    
+         DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          
+          DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+           
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          DrawPx(5,1,Blue);
+          DrawPx(7,1,Blue);
+          DrawPx(6,1,Blue);
+          
+          DrawPx(6,0,Blue);
+          DrawPx(7,1,Blue);
+          DrawPx(7,1,Blue);
+          DrawPx(5,1,Blue);    
+          DrawPx(6,2,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(13000, 50);
+          
+          DisplaySlate();
+          delay(150);
+          ClearSlate();      
+         
+          DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          
+          DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+           
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          
+          DrawPx(6,0,Blue);
+          DrawPx(5,0,Blue);
+          DrawPx(7,0,Blue);
+          
+          DrawPx(7,0,Blue);
+          
+          DrawPx(5,0,Blue);    
+          DrawPx(6,1,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(14000, 50);
+          
+          DisplaySlate();
+          delay(150);
+          ClearSlate();   
+      DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          
+          DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+           
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          
+             
+          DrawPx(6,0,Red);
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+         
+          Tone_Start(16000, 50);
+          
+          DisplaySlate();
+          delay(150);
+          ClearSlate(); 
+       DrawPx(1,0,Green);
+          DrawPx(2,0,Green);
+          DrawPx(3,0,Green);
+          DrawPx(4,0,Green);
+          
+          
+           DrawPx(0,0,Green);
+          DrawPx(3,2,White);
+          DrawPx(4,2,White);
+          
+          DrawPx(2,1,Green);
+          DrawPx(3,1,Green);
+          DrawPx(4,1,Green);
+          
+             
+
+          DrawPx(0,7,Yellow);
+          DrawPx(0,6,Yellow);
+          DrawPx(1,6,Yellow);
+          DrawPx(1,7,Yellow);
+          
+
+          
+         
+          Tone_Start(18000, 50);
+          
+          DisplaySlate();
+          delay(250);
+          ClearSlate();
+          
+          DrawPx(0,7,Red);
+          DrawPx(1,6,Red);
+          DrawPx(2,5,Red);
+          DrawPx(3,4,Red);
+          DrawPx(4,3,Red);
+          DrawPx(5,2,Red);
+          DrawPx(6,1,Red);
+          DrawPx(7,0,Red);
+          
+          DrawPx(0,0,Red);
+          DrawPx(1,1,Red);
+          DrawPx(2,2,Red);
+          DrawPx(3,3,Red);
+          DrawPx(4,4,Red);
+          DrawPx(5,5,Red);
+          DrawPx(6,6,Red);
+          DrawPx(7,7,Red);
+          Tone_Start(18183, 1000);
+          
+          DisplaySlate();
+          delay(1000);
+          
+          
+  Serial.println("Game over!");
+  //animates and delays for a while
+}
 
